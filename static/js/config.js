539fa1cef9615eda6c949e69c159333d77c866f1
liangyu09 <16492019+liangyu09@users.noreply.github.com>
2023-06-01 01:06:29 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: "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"  // openAi Base64 编码 apiKey
+    apiKey: "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"  // openAi Base64 编码 apiKey
 };  
